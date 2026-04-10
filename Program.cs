4b10e4b13f1ff8824f393b427e48a3868ffaa780
MB-git-creator <mukimovbehruz0@gmail.com>
2026-04-10 16:13:35 +0300

    commit2

--- Program.cs
+++ Program.cs
@@ -1,52 +1,4 @@
-﻿//Class task 1
-// int fact(int n)
-// {
-//     if (n <= 1)
-//     {
-//         return 1;
-//     }
-//     return n*fact(n-1);
-// }
-// System.Console.WriteLine(fact(9))
-
-//2
-
-// System.Int32 number = System.Int32.Parse(System.Console.ReadLine());
-// System.Int64 result = Fibonacci(number);
-// System.Console.WriteLine(result);
-
-// System.Int64 Fibonacci(System.Int32 n)
-// {
-//     if (n == 0)
-//         return 0;
-//     else if (n == 1)
-//         return 1;
-//     else
-//         return Fibonacci(n - 1) + Fibonacci(n - 2);
-// }
-
-//3
-// int fact(int n)
-// {
-//     if (n <= 1)
-//     {
-//         return 1;
-//     }
-//     return n*fact(n-2);
-// }
-// System.Console.WriteLine(fact(9));
-
-//home task 1
-//1
-// int n = int.Parse(Console.ReadLine());
-
-// for (int i = 1; i <= n; i++)
-// {
-//     Console.Write(i + " ");
-// }
-
-//2
-
+﻿
 // int n = int.Parse(Console.ReadLine());
 
 // for (int i = n; i >= 1; i--)
@@ -93,7 +45,7 @@
 //     return 1 + sum(num / 10);
 // }
 
-// int digitsCount = sum(numb);
-// Console.WriteLine(digitsCount);
+// int cham = sum(numb);
+// Console.WriteLine(cham);
 
 //6
